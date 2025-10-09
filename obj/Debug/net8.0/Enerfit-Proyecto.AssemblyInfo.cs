708591afa0e5f41286232d508204a0c63100b5fa
ALAN URIEL LAIZEROVICH <49518922@ort>
2025-10-09 15:54:12 -0300

    segunda actu

--- obj/Debug/net8.0/Enerfit-Proyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/Enerfit-Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enerfit-Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c69bf063218194dbfae24da8df44b8fe929acf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enerfit-Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enerfit-Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
