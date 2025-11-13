352e4d36b22719fe9db7fe1127f74cbb2e32d1ad
PEDRO MALOWICKI SANCHO <49552421@ort>
2025-11-13 15:58:44 -0300

    ultima hora prog 13/11

--- obj/Debug/net8.0/Enerfit-Proyecto.AssemblyInfo.cs
+++ obj/Debug/net8.0/Enerfit-Proyecto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enerfit-Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d88fc000d603bcedcca75904eda18fbda8989d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92d8b1184b425546e042b7eed7dc2a9d3efb4abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enerfit-Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enerfit-Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
